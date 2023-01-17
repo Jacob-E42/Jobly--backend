6c07048b3b8cd22817a3aa16e69774f01e5cec80
Jacob Eiferman <jacobeiferman@gmail.com>
2023-01-17 12:55:16 -0500

    updated config details

--- Jobly/config.js
+++ Jobly/config.js
@@ -11,15 +11,11 @@ const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
-  return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+	return process.env.NODE_ENV === "test" ? "jobly_test" : process.env.DATABASE_URL || "jobly";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
-//
-// WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
-const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 13;
 
 console.log("Jobly Config:".green);
 console.log("SECRET_KEY:".yellow, SECRET_KEY);
@@ -29,8 +25,8 @@ console.log("Database:".yellow, getDatabaseUri());
 console.log("---");
 
 module.exports = {
-  SECRET_KEY,
-  PORT,
-  BCRYPT_WORK_FACTOR,
-  getDatabaseUri,
+	SECRET_KEY,
+	PORT,
+	BCRYPT_WORK_FACTOR,
+	getDatabaseUri
 };
