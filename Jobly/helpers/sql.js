3b5dcf310a9429523df59e863fc9fc05c1da3d40
Jacob Eiferman <jacobeiferman@gmail.com>
2023-01-19 10:28:17 -0500

    added documentation for sqlForPartial Update

--- Jobly/helpers/sql.js
+++ Jobly/helpers/sql.js
@@ -1,20 +1,28 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/*
+  Take data provided in an object and use it to return a SQL appropriate string 
+  of specific columns to update in an UPDATE statement. 
+
+  {firstName, lastName} -> {setCols: `first_name=$1, last_name=$2`, values: ['John', 'Smith']}
+
+  An optional jsToSql paramter is an object which has the names of JS keys and the value set to the proper SQL equivalent.
+  Ex: {firstName: "first_name"}
+
+  If provided, the setCols string will use the SQL equivalent strings
+*/
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
-  const keys = Object.keys(dataToUpdate);
-  if (keys.length === 0) throw new BadRequestError("No data");
+	const keys = Object.keys(dataToUpdate);
+	if (keys.length === 0) throw new BadRequestError("No data");
 
-  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
-  );
+	// {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+	const cols = keys.map((colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`);
 
-  return {
-    setCols: cols.join(", "),
-    values: Object.values(dataToUpdate),
-  };
+	return {
+		setCols: cols.join(", "),
+		values: Object.values(dataToUpdate)
+	};
 }
 
 module.exports = { sqlForPartialUpdate };
